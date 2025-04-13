2d964ef14ccea1cc8174d3955d9b36dec28ef6c7
Auðun Valborgarson <audun@mms.is>
2025-04-13 12:11:20 +0000

    add get_time

--- R/students_responses.R
+++ R/students_responses.R
@@ -19,7 +19,8 @@
 #'   \item{start_time}{POSIXct, timestamp when the item was started}
 #'   \item{end_time}{POSIXct, timestamp when the item was submitted}
 #' }
-#'
+#' @importFrom dplyr select mutate transmute arrange  filter distinct
+#' @importFrom tidyr pivot_wider
 #' @examples
 #' \dontrun{
 #' formatted <- format_responses(raw_long_response_data)
@@ -37,18 +38,43 @@ format_responses <- function(responses) {
   responses |>
     dplyr::left_join(item_id, by =  dplyr::join_by(item_number)) |>
     tidyr::pivot_wider(names_from = "var", values_from = "val") |>
+    dplyr::distinct() |>
     dplyr::transmute(
       student_id = as.character(login),
       item_id = as.character(item_id),
       item_number = as.numeric(item_number),
       response = responses_response_value,
-      response_status = factor(status_correct),
-      score = as.numeric(outcomes_score),
+      response_status = (status_correct),
+      score = as.numeric(score),
       response_time = as.numeric(duration),
-      start_time = as.POSIXct(session_start_time),
-      end_time = as.POSIXct(session_end_time)
+      start_time = get_time(item_start_time),
+      end_time = get_time(item_end_time)
     ) |>
     dplyr::arrange(item_id, student_id)
+}
 
+
+
+
+#' Convert timestamp in milliseconds to POSIXct format
+#'
+#' This function converts a timestamp in milliseconds (Unix time) to POSIXct format, which
+#' can be used to represent dates and times in R. The timestamp is assumed to be in UTC.
+#'
+#' @param timestamp A numeric or character vector containing the timestamp in milliseconds.
+#'   This will be converted into seconds before being used to generate the corresponding POSIXct object.
+#'
+#' @return A POSIXct object representing the timestamp in UTC.
+#'
+#' @examples
+#' timestamp <- 1620000000000
+#' converted_time <- get_time(timestamp)
+#' print(converted_time)
+#'
+#' @export
+get_time <- function(timestamp) {
+  timestamp_sec <- as.numeric(timestamp) / 1000
+  converted_time <- as.POSIXct(timestamp_sec, origin = "1970-01-01", tz = "UTC")
+  return(converted_time)
 }
 
